c43ae56d65c37236aa3856d8fb6bcd93ce720723
Fef0 <19290442+Fef0@users.noreply.github.com>
2022-11-11 21:29:22 +0100

    chore: Start work towards context switching + minor tweaks

--- Core/Src/scheduler.c
+++ Core/Src/scheduler.c
@@ -8,12 +8,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "scheduler.h"
+#include "stm32f4xx.h"
+
+#define BUS_FREQ   16000000
+#define MILLIS_PRESCALER BUS_FREQ/1000
 
 // It's a linked list of Task Control Blocks
 tcbListType* tasksList;
 
 // Points to the current task
 tcbNode* currentTask;
+
 
 void initList() {
 	// Dynamically allocate space for tasksList and init it
@@ -27,8 +32,8 @@ uint8_t isListEmpty() {
 	return tasksList->tasksNum == 0;
 }
 
-// The stack will contain {R0-R12}, LR, PC, xPSR
-// but not in this order
+// The stack will contain {R0-R12}, LR, PC, xPSR and eventually the Floating Point registers {s0-s31}
+// (not in this order)
 uint32_t* createStack(void (*taskFunc)()) {
 	// Dynamically create the new stack and update the pointer
 	uint32_t* stackPt = (uint32_t*) malloc(sizeof(stack));
@@ -46,13 +51,13 @@ uint32_t* createStack(void (*taskFunc)()) {
 }
 
 // Add a new task to the tasks list
-void addTask(void (*taskFunc)(), uint8_t taskID) {
+int addTask(void (*taskFunc)(), uint8_t taskID) {
 	// Allocate space for a new Thread Control Block
 	tcbNode* t = (tcbNode*) malloc(sizeof(tcbNode));
 	// Create the stack
 	t->stackPt = createStack(taskFunc);
 	// Set taskID
-	t->taskID = taskID;
+	t->taskID++;
 
 	// If the list is empty then initialize head and tail at the same position
 	// Otherwise update the last element to point to the new one and update the tail
@@ -70,6 +75,11 @@ void addTask(void (*taskFunc)(), uint8_t taskID) {
 	tasksList->tail->next = tasksList->head;
 	// Update tasks number
 	tasksList->tasksNum++;
+
+	tcbNode* test = currentTask;
+
+	// Return taskID
+	return t->taskID;
 }
 
 /*
@@ -96,11 +106,19 @@ uint8_t removeTask(uint8_t taskID) {
 }
 */
 
-void task0() {
-	int z = 0;
+int c1 = 0;
+int c2 = 0;
 
+void task0() {
 	while(1) {
-		z++;
+		c1++;
+		SysTick_Handler();
+	}
+}
+
+void task1() {
+	while(1) {
+		c2++;
 	}
 }
 
@@ -109,8 +127,20 @@ void startScheduler() {
 	initList();
 
 	// Add the first task
-	addTask(&task0, 1);
+	int id1 = addTask(&task0, 1);
 
+	// Add the first second
+	int id2 = addTask(&task1, 2);
+
+	uint32_t* addr = (uint32_t*) &task1;
+
+	// Config systick
+	SysTick->CTRL =0;   //Disable the SysTick timer; Offset: 0x000 (R/W)  SysTick Control and Status Register
+	SysTick->VAL=0;     //Clear current value to 0; Offset: 0x008 (R/W)  SysTick Current Value Register
+	//NVIC_SetPriority(SysTick_IRQn, 0x0);   // This is not necessary because we don't implement other interrupt.
+	SysTick->LOAD = (5 * MILLIS_PRESCALER)-1;   //Offset: 0x004 (R/W)  SysTick Reload Value Register
+	// Enable systick
+	SysTick->CTRL =0x00000007;
 
 	// Call SVC_Handler and load the first task
 	__asm("svc 0");
