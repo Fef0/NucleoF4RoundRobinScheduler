1033c09ab6237bd87e9be180b543e5a1f0450f87
Fef0 <19290442+Fef0@users.noreply.github.com>
2022-11-11 21:31:23 +0100

    chore: Changed SysTick_Handler name in stm32f4xx_it.c in order to avoid overriding of my custom SysTick_Handler

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -180,7 +180,7 @@ void PendSV_Handler(void)
 /**
   * @brief This function handles System tick timer.
   */
-void SysTick_Handler(void)
+void SysTick_Handler_not_used(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
