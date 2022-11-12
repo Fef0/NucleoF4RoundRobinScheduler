22a340482ced9e0a8915478604ae92e93ca31810
Fef0 <19290442+Fef0@users.noreply.github.com>
2022-11-12 16:28:47 +0100

    chore: Change how SVC_Handler and SysTick_Handler work in order to use Process Stack Pointer and Thread mode

--- Core/Src/context_switch.s
+++ Core/Src/context_switch.s
@@ -23,93 +23,108 @@
 // Variable import
 .global currentTask
 
+
 // Used to start the first task
+// Since this function is an interrupt, {R0-R3, LR, PC, xPSR} have already been pushed to stack
 SVC_Handler:
+
 	// Disable interrupts
-    cpsid I
-
-    // Load pointer to currentTask, which is a pointer (R0 = &currentTask)
+    cpsid i
+    // Load currentTask pointer (R0 = &currentTask)
     ldr R0, =currentTask
-    // Dereference to get currentTask (R0 = *(&currentTask) = currentTask) which is a pointer to the tcb
+
+    // Dereference &currentTask (R0 = *(&currentTask) = currentTask) which is
+    // a pointer to the task tcb
     ldr R0, [R0]
-	// Dereference one last time to get the tcb that currentTask points to
-	// The first element of tcb struct is the stackPt (SP = currentTask->stackPt)
-	// Now SP points to the first full cell of the task stack, the PC register
-    ldr SP, [R0]
 
-	// Pop the PC register from the task stack into the CPU LR register in order to return from the SVC_Handler
-	// directly inside the first task code
-	pop {LR}
+    // Dereference &tcb to get tcb->stackPt
+    ldr R0, [R0]
 
-    cpsie I // Enable interrupts
+    // Pop the software stack registers and update tcb->stackPt (which is the task stack pointer)
+    // in this case LR will contain 0xFFFFFFD which tells the CPU to return to
+    // the hardware stack fram LR (which will be pop later) with thread mode
+    // and using psp for the task stack
+    ldmfd R0!, {R4-R11, LR}
 
-    bx LR // Return from the function and start the first task
+	// Update PSP to point to R0 (which is the task stack pointer)
+    msr PSP, R0
+
+    // Enable interrupts
+    cpsie i
+
+    // Branch and exchange will load software LR value, tells the CPU
+    // to use thread mode and psp and then pop the hardware stack frame
+    // from the psp in order to branch to the first task
+    bx LR
 
 
-// TODO fix hardfault from branch
+// Used to handle the context switch from one task to another
+// Since this function is an interrupt, {R0-R3, LR, PC, xPSR} have already been pushed to stack
 SysTick_Handler:
+	// REMEMBER: every edit done before switching task
+	// is done on currentTask stack
+
 	// Disable interrupt
 	cpsid I
 
-	// CPU already saved the stack frame ({r0-r3}, r12, lr, pc, xpsr)
-	// and eventually {s0-s15} if we're using Floating Points
-	// so we have to push {r4-r11} to task stack (and {s16-s31}) and then switch to the next stack
-	// (Remember that currently SP is pointing to the currentTask stack)
-	push {R4-R11}
+	// Save the PSP in R0 in order to manipulate it
+	mrs R0, PSP
 
-	// Check if we're inside a Floating Point Context
-	// by checking LR content (after an interrupt it gets updated with useful info)
-	// In particular if the 5th bit is 1 then FPC is active so we must push the floating
-	// registers too
-	//tst LR, 0x10
+	// Load current task pointer (R1 = &currentTask)
+	ldr R1, =currentTask
+	// Dereference (R1 = *(&currentTask) = currentTask = &tcb) which is
+	// a pointer to the tcb
+	ldr R2, [R1]
+
+	// Dereference again to get tcb (R2 = *(&tcb) = tcb) but since it is a struct
+	// and its first element is stackPt, then R2 = tcb = tcb->stackPt
+	ldr R2, [R2]
+
+	// Check if the current context is a Floating Point Context
+	// (see cortex m4 manual)
+	//tst LR, #0x00000010
+	// if equal then
 	//it eq
-	//push {s16-s31}
+	// If equal, then push FPU registers into currentTask stack
+	//vstmdbeq  R0!, {S16-S31}
 
-	// Get currentTask address (R0 = &currentTask)
-	ldr R0, =currentTask
+	// Push normal context registers into currentTask stack
+	stmfd R0!, {R4-R11, LR}
 
-	// Dereference, so R0 = *(&currentTask) = currentTask
-	ldr R0, [R0]
+	// Save the new stack pointer inside currentTask stack
+	// in order to be used for the next task run
+	str R0, [R2]
 
-	// Dereference again, but this time in a different register so we can save
-	// currentStack address
-	// We are going to add 4 in order to dereference and get tcb->next at once
-	// (r1 = currentTask->next)
-	ldr R1, [R0, #4]
-	ldr R2, [R0]
+	ldr r2, [r1]
 
-	// Now R0 contains currentTask, which is a pointer
-	// And R1 contains currentTask>next, which is a pointer too
+	// ---- Load next task ----
+	// Load next task pointer starting from currentTask
+	// (R3 = (&tcb->next)) which is a pointer to another tcb
+	// (since it's a uint32_t then the "next" element is 4 bytes after "stackPt")
+	ldr R3, [R2, #4]
 
-	// Override currentTask with currentTask->next
-	// (currentTask = currentTask->next)
-	str R1, [R0]
+	// Dereference to get tcb->next (R3 = *(&(tcb->next)) = tcb->next) which is equal to
+	// tcb->next->stackPt and load it into R0 (R0 = tcb->next->stackPt)
+	ldr R0, [R3]
 
-	// Change stack pointer to point to the currentTask
-	ldr SP, [R1]
+	// Make currentTask point to currentTask->next = tcb->next
+	// currentTask = tcb->next
+	str R3, [R1]
 
-	// We are assuming that we pushed {R4-R11} and maybe {S16-S31} in the latest iteration, so we have to pop them
-	// from the stack
-	//tst LR, 0x10
+	// Pop R4-R11 and LR from the next task stack
+	ldmfd R0!, {R4-R11, LR}
+
+	// Check if the current context is a Floating Point Context
+	// (see cortex m4 manual)
+	//tst LR, #0x00000010
+	// if equal then
 	//it eq
-	//pop {S16-S31}
+	// If equal, then push FPU registers into currentTask stack
+	//vldmiaeq  R0!, {S16-S31}
 
-	pop {R4-R11}
+	// PSP will now point to the next task stack
+	msr PSP, R0
 
-	// Now SP points to the PC, so we must pop it to lr
-	pop {LR}
-
-	// Enable interrupt
 	cpsie I
-
-	// And now we can branch to the next task
-	bx LR;
-
-
-
-
-
-
-
 
 
