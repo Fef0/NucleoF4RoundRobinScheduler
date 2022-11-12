705a67dc2523f068883b2aebd1a61f8e5a0e0dd6
Fef0 <19290442+Fef0@users.noreply.github.com>
2022-11-12 16:27:38 +0100

    chore: Change how stack is created and managed + more testing

--- Core/Src/scheduler.c
+++ Core/Src/scheduler.c
@@ -19,6 +19,9 @@ tcbListType* tasksList;
 // Points to the current task
 tcbNode* currentTask;
 
+void taskReturn() {
+	while(1);
+}
 
 void initList() {
 	// Dynamically allocate space for tasksList and init it
@@ -32,6 +35,38 @@ uint8_t isListEmpty() {
 	return tasksList->tasksNum == 0;
 }
 
+void initStack(uint32_t* stackPt, void (*taskFunc)()) {
+	// STACK FRAME {R0-R3, R12, LR, PC, xPSR}
+	// xPSR default (from Cortex m4 manual)
+	stackPt[STACKSIZE-1] = 0x01000000;
+	// PC will point to the function itself
+	stackPt[STACKSIZE-2] = (uint32_t) taskFunc;
+	// stack frame LR will contain a default return address
+	stackPt[STACKSIZE-3] = (uint32_t) taskReturn;
+	// Init R12, R3, R2, R1, R0
+	// R12: stackPt[STACKSIZE-4] = 0
+	// R3: stackPt[STACKSIZE-5] = 0
+	// ...
+	// R0: stackPt[STACKSIZE-8] = 0
+	for (int i=4; i<9; i++) {
+		stackPt[STACKSIZE-i] = 0;
+	}
+
+	// Software LR will contain an EXC_RETURN that tells
+	// the processor to switch to thread mode and to use PSP
+	// (from cortex manual)
+	stackPt[STACKSIZE-9] = 0xFFFFFFFD;
+
+	// Init the rest of the software stack {R4-R11}
+	// R11: stackPt[STACKSIZE-10] = 0
+	// ...
+	// R4: stackPt[STACKSIZE-17] = 0
+	for (int i=10; i<=17; i++) {
+		stackPt[STACKSIZE-i] = 0;
+	}
+}
+
+
 // The stack will contain {R0-R12}, LR, PC, xPSR and eventually the Floating Point registers {s0-s31}
 // (not in this order)
 uint32_t* createStack(void (*taskFunc)()) {
@@ -39,16 +74,15 @@ uint32_t* createStack(void (*taskFunc)()) {
 	uint32_t* stackPt = (uint32_t*) malloc(sizeof(stack));
 
 	// Fill the stack with default values
-	// xPSR default (from Cortex m4 manual)
-	stackPt[STACKSIZE-1] = 0x01000000;
-	// PC will point to the function itself
-	stackPt[STACKSIZE-2] = (uint32_t) taskFunc;
+	initStack(stackPt, taskFunc);
 
 	// The stack will grow from the bottom up so
-	// the stack pointer must point to the PC which is
-	// the last full cell of the stack
-	return &(stackPt[STACKSIZE-2]);
+	// the stack pointer must point to the latest element (top of stack)
+	return &(stackPt[STACKSIZE-17]);
 }
+
+tcbNode* t1;
+tcbNode* t2;
 
 // Add a new task to the tasks list
 int addTask(void (*taskFunc)(), uint8_t taskID) {
@@ -66,17 +100,17 @@ int addTask(void (*taskFunc)(), uint8_t taskID) {
 		// Update currentTask pointer to point to the first
 		// task added
 		currentTask = t;
+		t1 = t;
 	} else {
 		tasksList->tail->next = t;
 		tasksList->tail = t;
+		t2 = t;
 	}
 
 	// The last element will now point to the head of the circular list
 	tasksList->tail->next = tasksList->head;
 	// Update tasks number
 	tasksList->tasksNum++;
-
-	tcbNode* test = currentTask;
 
 	// Return taskID
 	return t->taskID;
@@ -112,7 +146,6 @@ int c2 = 0;
 void task0() {
 	while(1) {
 		c1++;
-		SysTick_Handler();
 	}
 }
 
@@ -127,18 +160,15 @@ void startScheduler() {
 	initList();
 
 	// Add the first task
-	int id1 = addTask(&task0, 1);
+	addTask(&task0, 1);
 
 	// Add the first second
-	int id2 = addTask(&task1, 2);
-
-	uint32_t* addr = (uint32_t*) &task1;
+	addTask(&task1, 2);
 
 	// Config systick
 	SysTick->CTRL =0;   //Disable the SysTick timer; Offset: 0x000 (R/W)  SysTick Control and Status Register
 	SysTick->VAL=0;     //Clear current value to 0; Offset: 0x008 (R/W)  SysTick Current Value Register
-	//NVIC_SetPriority(SysTick_IRQn, 0x0);   // This is not necessary because we don't implement other interrupt.
-	SysTick->LOAD = (5 * MILLIS_PRESCALER)-1;   //Offset: 0x004 (R/W)  SysTick Reload Value Register
+	SysTick->LOAD = (0.01 * MILLIS_PRESCALER)-1;   //Offset: 0x004 (R/W)  SysTick Reload Value Register
 	// Enable systick
 	SysTick->CTRL =0x00000007;
 
