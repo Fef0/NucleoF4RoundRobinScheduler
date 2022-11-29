5f31088cdae6af242f5f383d0e207f992755f4ea
Fef0 <19290442+Fef0@users.noreply.github.com>
2022-11-29 11:39:03 +0100

    chore: Update CPU frequency and change prescaler to consider microseconds instead of milliseconds

--- Core/Inc/scheduler.h
+++ Core/Inc/scheduler.h
@@ -4,17 +4,15 @@
  *  Created on: Nov 6, 2022
  *      Author: fef0
  */
-
-#define MAXTASKS 10
+#define BUS_FREQ 84000000
+#define MICRO_PRESCALER BUS_FREQ/1000000
 #define STACKSIZE 256
 
-// A stack will contain the register starting from the 128-16 = 112th position in this order:
-// {r12-r0}, LR, PC, xPSR
+// A stack will contain the task registers
 typedef uint32_t stack[STACKSIZE];
 
 // Task Control Block will point to its relative stack pointer
-// And will be linked to the next thread control block
-// And the previous one (will speed up hops)
+// And will be linked to the next task control block
 struct tcb {
 	uint32_t* stackPt;
 	struct tcb* next;
@@ -37,8 +35,9 @@ uint8_t isListEmpty();
 uint32_t* createStack();
 void addTask(void (*taskFunc)());
 void idleTask();
-void clockInit(uint32_t quanta);
+void sysTickInit(uint32_t quanta_us);
 void task0();
 void task1();
+void initScheduler(uint32_t quanta_us);
 void startScheduler();
 
